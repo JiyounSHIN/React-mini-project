56780c50d205acbbb32f9b23e3261da17b0f6b7f
ssjeu <sje1015s@gmail.com>
2022-06-14 13:50:34 +0900

    메인화면 추가 (카테고리 수정 전)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import SignUp from './page/signup';
 import LogIn from './page/login';
 import Postwrite from './page/postwrite';
 import Postupdate from './page/postupdate';
+import Main from './page/main';
+import Posts from './page/posts';
+import Post from './page/post';
 import './App.css';
 import { Link, Routes, Route } from "react-router-dom";
 
@@ -14,10 +17,13 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/posts" element={<Posts />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/postwrite" element={<Postwrite />} />
         <Route path="/postupdate" element={<Postupdate />} />
+        <Route path="/post" element={<Post />} />
       </Routes>
     </div>
   );
